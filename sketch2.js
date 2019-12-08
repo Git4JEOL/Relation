0e80e298ff65f95b7a7f6cf74238b6ffecf6ede4
Git4JEOL <claener@naver.com>
2019-12-09 04:42:01 +0900

    Update sketch2.js
    add mouseX interaction

--- sketch2.js
+++ sketch2.js
@@ -2,16 +2,34 @@ var player_sprite_sheet;
 var player_walk;
 var player_stand;
 var player_sprite;
-
+var mouse_moved;
+var mouse_clicked=false;
 function setup(){
   createCanvas(500,500);
   player_sprite= createSprite(100,284,10,10)
-
+  frameRate(60);
 }
 function draw() {
   clear();
   background(0);
-  player_sprite.velocity.x=2;
-  drawSprites();
 
+  var eventX;
+  if(mouse_moved==true){
+    eventX = mouseX;
+    }
+
+  if(eventX > player_sprite.position.x - 10) {
+    player_sprite.velocity.x=2;
+  }
+  if(eventX < player_sprite.position.x + 10) {
+    player_sprite.velocity.x=-2;
+  }
+
+//if player go outside
+
+  drawSprites();
+}
+
+function mouseMoved(){
+    mouse_moved = true;
 }
