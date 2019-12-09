b563e177c283e31d4588d66685bec4b5d500f671
Git4JEOL <claener@naver.com>
2019-12-10 03:33:13 +0900

    Update sketch2.js
    made random generate object

--- sketch2.js
+++ sketch2.js
@@ -2,21 +2,33 @@ var player_sprite_sheet;
 var player_walk;
 var player_stand;
 var player_sprite;
+var fellow;
+var fellow_sprite;
+var objects;
 var object_sprite;
 var objectX;
 var objectY;
 var mouse_moved;
 var mouse_clicked=false;
+
 function setup(){
+  objects=new Group(mate);
   createCanvas(500,500);
+ for (var i = 0; i < 10; i++){
+   var mate =object_sprite=createSprite(random(10,490),random(10,490),10,10)
+    objects.add(mate);
+ }
+
   player_sprite= createSprite(100,284,10,10)
-  object_sprite=createSprite(100,284,10,10)
+
+
+
   frameRate(60);
 }
 function draw() {
   clear();
   background(0);
-
+  player_sprite.overlap(objects,collect)
   var eventY;
   var eventX;
   if(mouse_moved==true){
@@ -31,7 +43,6 @@ function draw() {
     player_sprite.velocity.x=-2;
   }
   else {
-    player_sprite.changeAnimation('stand');
     //if close to the mouse, don't move
     player_sprite.velocity.x = 0;
   }
@@ -42,7 +53,6 @@ function draw() {
     player_sprite.velocity.y=+2;
   }
   else {
-    player_sprite.changeAnimation('stand');
     //if close to the mouse, don't move
     player_sprite.velocity.x = 0;
   }
@@ -54,4 +64,8 @@ function draw() {
 
 function mouseMoved(){
     mouse_moved = true;
+}
+function collect(collector,collected){
+  collected.remove();
+  print("1");
 }
